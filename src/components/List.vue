47ee1b6e873863d79a17612d689f4f352fecb780
Hari Ramilison <hramilison@unomaha.edu>
2020-09-02 14:16:33 -0600

    add responsive table

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,7 +3,7 @@
         <div class="col-md-12">
             <table class="table table-striped">
                 <thead>
-                    <tr>
+                    <tr class="d-none d-sm-table-row">
                         <th>Name</th>
                         <th>Description</th>
                         <th>Amount</th>
@@ -11,11 +11,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="subscription in Subscriptions" :key="subscription.key">
-                        <td>{{ subscription.name }}</td>
-                        <td>{{ subscription.description }}</td>
-                        <td>{{ subscription.amount }}</td>
-                        <td>
+                    <tr class="d-block d-sm-table-row" v-for="subscription in Subscriptions" :key="subscription.key">
+                        <td class="d-block d-sm-table-cell" data-title="Name">{{ subscription.name }}</td>
+                        <td class="d-block d-sm-table-cell" data-title="Description">{{ subscription.description }}</td>
+                        <td class="d-block d-sm-table-cell" data-title="Amount">{{ subscription.amount }}</td>
+                        <td class="d-block d-sm-table-cell">
                             <router-link :to="{name: 'edit', params: { id: subscription.key }}" class="btn btn-primary">Edit
                             </router-link>
                             <button @click.prevent="deleteSubscription(subscription.key)" class="btn btn-danger">Delete</button>
@@ -60,3 +60,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ @media(max-width: 580px){
+    [data-title]::before {
+      content: attr(data-title) ": ";
+      font-weight: bold;
+    }
+ }
+</style>
